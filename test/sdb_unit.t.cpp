87b961015bd004109436b3497691b777678ebafe
Okwudili Pat-Nebe <15070069+patnebe@users.noreply.github.com>
2025-08-20 13:31:38 +0100

    [Chore] Switch from Catch2 to gtest (#7)
    #### Context
    * Unit testing scaffold
    
    #### Changes
    * Switch from Catch2 to gtest
    
    #### Test Plan
    * CI

---
+++ test/sdb_unit.t.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+
+TEST(HelloTest, BasicAssertions) {
+  EXPECT_EQ(7*6, 42);
+}
--- test/tests.cpp
+++
@@ -1,5 +0,0 @@
-int main()
-{
-  // No tests implemented. Placeholder main.
-  return 0;
-}
